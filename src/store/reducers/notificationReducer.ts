19325f38a18d5c63606bc8b742c1b625e3410983
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-15 14:33:40 +0330

    add types to notfication reducer and create initialState

--- src/store/reducers/notificationReducer.ts
+++ src/store/reducers/notificationReducer.ts
@@ -1,4 +1,11 @@
-export default (state , action) => {
+import { NotificaionState , NotificationAction } from "../types"
+
+const initialState: NotificaionState = {
+    message: '',
+    type: 'success'
+}
+
+export default (state = initialState , action: NotificationAction): NotificaionState => {
     switch(action.type) {
         default: 
         return state
