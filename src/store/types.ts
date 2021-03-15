e44f86cd3745584b051771beb4bbf7ff6b8ff608
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-15 11:54:09 +0330

    add types

---
+++ src/store/types.ts
@@ -0,0 +1,19 @@
+export const GET_LIST = 'GET_LIST';
+export const GET_LIST_BY_ID =  'GET_LIST_BY_ID';
+export const ADD_LIST ='ADD_LIST';
+export const DELETE_LIST = 'DELETE_LIST';
+export const UPDATE_LIST = 'UPDATE_LIST';
+export const SET_LISTID_TO_DELETE = 'SET_LIST';
+export const SET_LIST_TO_EDIT = 'SET_LIST_TO_EDIT';
+export const SET_SELECTED_LIST = 'SET_SELECTED_LIST';
+
+export const ADD_TASK = 'ADD_TASK';
+export const DELETE_TASK = 'DELETE_TASK';
+export const SET_TASK_TO_DELETE = 'SET_TASK_TO_DELETE';
+export const UNSET_TASK_TO_DELETE = 'UNSET_TASK_TO_DELETE';
+export const UPDATE_TASK = "UPDATE_TASK";
+export const SET_TASK_TO_EDIT = 'SET_TASK_TO_EDIT';
+export const UNSET_TASK_TO_EDIT = 'UNSET_TASK_TO_EDIT';
+
+export const SET_NOTFICATION = 'SET_NOTIFICATION';
+
