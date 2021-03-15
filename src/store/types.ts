132cd9cdf5bc1572f2db020028a6b33016930af9
sadeghrafiei <74002994+sadeghrafiei@users.noreply.github.com>
2021-03-15 12:20:02 +0330

    create action types with  interface

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,4 @@
-export const GET_LIST = 'GET_LIST';
+export const GET_LISTS = 'GET_LISTS';
 export const GET_LIST_BY_ID =  'GET_LIST_BY_ID';
 export const ADD_LIST ='ADD_LIST';
 export const DELETE_LIST = 'DELETE_LIST';
@@ -31,4 +31,104 @@ export interface List {
 
 export interface Lists {
     [id: string]: List
+}
+
+interface AddListAction {
+    type: typeof ADD_LIST;
+    payload: List;
+}
+
+interface GetListsAction {
+    type: typeof GET_LISTS;
+}
+
+interface GetListByIdAction {
+    type: typeof GET_LIST_BY_ID;
+    payload: string;
+}
+
+interface SetListIdToDeleteAction {
+    type: typeof SET_LISTID_TO_DELETE;
+    payload: string;
+}
+
+interface SetListToEditAction {
+    type: typeof SET_LIST_TO_EDIT;
+    payload: string;
+}
+
+interface DeleteListAction {
+    type: typeof DELETE_LIST;
+    payload: string;
+}
+
+interface UpdateListAction {
+    type: typeof UPDATE_LIST;
+    payload: {
+        id: string;
+        name: string;
+    }
+}
+
+interface SetSelectedListAction {
+    type: typeof SET_SELECTED_LIST;
+    payload: string
+}
+
+interface AddTaskAction {
+    type: typeof ADD_TASK;
+    payload: {
+        task: Task;
+        list: List
+    }
+}
+
+interface DeleteTaskAction {
+    type: typeof DELETE_TASK;
+    payload: {
+        task: Task;
+        list: List;
+    }
+}
+
+interface SetTaskToDeleteAction {
+    type: typeof SET_TASK_TO_DELETE;
+    payload: {
+        task: Task;
+        list: List;
+    }
+}
+
+interface UnsetTaskToDeleteAction {
+    type: typeof UNSET_TASK_TO_DELETE;
+}
+
+interface EditTaskAction {
+    type: typeof UPDATE_TASK;
+    payload: {
+        taskId: string;
+        taskName: string;
+        taskState: boolean;
+        list: List
+    }
+}
+
+interface SetTaskToEditAction {
+    type: typeof SET_TASK_TO_EDIT;
+    payload: {
+        task: Task;
+        list: List;
+    }
+}
+
+interface UnsetTaskToEditAction {
+    type: typeof UNSET_TASK_TO_EDIT;
+}
+
+interface SetNotificationAction {
+    type: typeof SET_NOTFICATION;
+    payload: {
+        msg: string;
+        type: string;
+    }
 }
